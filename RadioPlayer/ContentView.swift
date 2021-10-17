a881c193e5474e5af3692b03bcbe41a16cfa69d7
Adem Özcan <ozcanfurkan@gmail.com>
2021-10-17 14:29:32 +0300

    create swiftui project

---
+++ RadioPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RadioPlayer
+//
+//  Created by Adem Özcan on 17.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RadioPlayer/RadioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioPlayerApp.swift
+//  RadioPlayer
+//
+//  Created by Adem Özcan on 17.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct RadioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
